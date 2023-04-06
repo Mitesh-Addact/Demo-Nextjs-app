7562a2a21700aa67608bc0aa6408972d920070fb
Mitesh <mitesh@addact.net>
2023-04-06 12:47:38 +0530

    Demo application
    Next.js application

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <p>Mitesh Demo Application</p>
+    </div>
+  )
+}
+
+export default HomePage
