7914153538eb98da5e467bcd6bfbc80e02c74a88
Mitesh <mitesh@addact.net>
2023-04-06 15:42:12 +0530

    demo change
    demo changes

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to my Next.js application</h1>
+      <h1>Welcome to Addact my Next.js application</h1>
       <p>Mitesh Demo Application</p>
       <span>Testing completed</span>
     </div>
